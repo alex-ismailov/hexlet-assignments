ad28d22cac12d0b0e04d82a506e977761fd5d0d5
alex-ismailov <alexx.ismailov@gmail.com>
2023-07-22 08:47:49 -0400

    [les 5 models] Reinit exercise

--- rails-basics-ru/models/test/controllers/articles_controller_test.rb
+++ rails-basics-ru/models/test/controllers/articles_controller_test.rb
@@ -2,14 +2,20 @@
 
 require 'test_helper'
 
-class ArticlesControllerTest < ActionDispatch::IntegrationTest
+class ArticlesTest < ActionDispatch::IntegrationTest
+  setup do
+    @article = articles(:one)
+  end
+
   test 'should get index' do
-    get articles_index_url
+    get articles_url
+
     assert_response :success
   end
 
-  test 'should get show' do
-    get articles_show_url
+  test 'should show article' do
+    get article_url @article
+
     assert_response :success
   end
 end
