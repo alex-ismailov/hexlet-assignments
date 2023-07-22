bfae90d1e6e23c5a014673fdb9c3c1b5ac821d41
alex-ismailov <alexx.ismailov@gmail.com>
2023-07-21 21:58:40 -0400

    [les 5 models] add solution

--- rails-basics-ru/models/config/routes.rb
+++ rails-basics-ru/models/config/routes.rb
@@ -3,7 +3,8 @@
 Rails.application.routes.draw do
   root 'home#index'
   # BEGIN
-  
+  resources :articles, only: :index
+  resources :articles, only: :show
   # END
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
