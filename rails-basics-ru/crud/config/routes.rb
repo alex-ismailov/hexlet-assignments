feba5708fe3bc22a5c3d37353e4267cb43d6e253
@hexlet/cli <support@hexlet.io>
2023-07-28 23:17:37 -0400

    submit rails-basics-ru/crud

--- rails-basics-ru/crud/config/routes.rb
+++ rails-basics-ru/crud/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # resources :tasks
   root 'home#index'
   # BEGIN
-  resources :tasks, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :tasks, only: %i[index show new create edit update destroy]
   # END
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
