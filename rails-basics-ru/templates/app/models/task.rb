3d201bee639c10bf19fa2f66a726f193e57a4b80
@hexlet/cli <support@hexlet.io>
2023-08-02 21:32:58 -0400

    submit rails-basics-ru/templates

--- rails-basics-ru/templates/app/models/task.rb
+++ rails-basics-ru/templates/app/models/task.rb
@@ -1,11 +1,6 @@
+# frozen_string_literal: true
+
 class Task < ApplicationRecord
   validates :name, :status, :creator, presence: true
   validates :completed, inclusion: [true, false]
 end
-
-# name - обязательное, строка. Название задачи
-# description - необязательное. Описание задачи
-# status - обязательное, строка. По умолчанию задача создается в новом статусе
-# creator - обязательно, строка. Создатель задачи
-# performer - необязательное, строка. Тот на кого поставлена задача
-# completed - обязательное, булево значение
