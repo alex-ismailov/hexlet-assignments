26b6ce90d6b928514072addfe7b76430ff6bc4ce
@hexlet/cli <support@hexlet.io>
2023-08-12 22:29:18 -0400

    submit rails-basics-ru/relations

--- rails-basics-ru/relations/app/models/status.rb
+++ rails-basics-ru/relations/app/models/status.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class Status < ApplicationRecord
   # BEGIN
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
 
   validates :name, presence: true
   # END
--- rails-basics-ru/relations/app/models/task.rb
+++ rails-basics-ru/relations/app/models/task.rb
@@ -3,5 +3,5 @@ class Task < ApplicationRecord
   belongs_to :user
   belongs_to :status
 
-  validates :name, :description, :user_id, :status_id, presence: true
+  validates :name, :user_id, :status_id, presence: true
 end
