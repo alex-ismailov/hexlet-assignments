1d25361e77a266676384e46728d2ce43c2713214
@hexlet/cli <support@hexlet.io>
2023-08-26 00:46:32 -0400

    submit rails-basics-ru/i18n

---
+++ rails-basics-ru/i18n/config/routes.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  # root ''
+  # BEGIN
+  # root 'home#index'
+
+  # scope ('/:locale'), locale: /en|ru/ do
+  #   resources :posts do
+  #     scope module: :posts do
+  #       resources :comments, only: %i[new edit create update destroy]
+  #     end
+  #   end
+  # end
+
+  # get "/", to: 'home#index'
+  root 'home#index'
+
+  scope ('/:locale'), locale: /en|ru/ do
+    resources :posts do
+      scope module: :posts do
+        resources :comments
+      end
+    end
+  end
+  # scope ('/:locale'), locale: /en|ru/ do
+  #   resources :posts do
+  #     scope module: :posts do
+  #       resources :comments, only: %i[create new]
+  #     end
+  #   end
+
+  #   resources :posts, shallow: true do
+  #     scope module: :posts, shallow_prefix: 'post' do
+  #       resources :comments, only: %i[destroy edit update]
+  #     end
+  #   end
+  # end
+  # END
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
