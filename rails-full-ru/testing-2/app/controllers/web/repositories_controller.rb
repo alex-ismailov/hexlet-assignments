b85a8b20b8e8daf4fc881d0519549dad77eb78e0
@hexlet/cli <support@hexlet.io>
2023-11-05 00:39:42 -0400

    submit rails-full-ru/testing-2

--- rails-full-ru/testing-2/app/controllers/web/repositories_controller.rb
+++ rails-full-ru/testing-2/app/controllers/web/repositories_controller.rb
@@ -17,47 +17,75 @@ class Web::RepositoriesController < Web::ApplicationController
     @repository = Repository.find params[:id]
   end
 
+  # def create
+  #   # BEGIN
+  #   @link = permitted_params[:link]
+  #   @client = Octokit::Client.new
+  #   @octokit_repo = Octokit::Repository.from_url(@link)
+  #   @repository_data = @client.repository(@octokit_repo)
+  #   @new_repo_params = {
+  #     link: @link,
+  #     owner_name: @repository_data.owner.login,
+  #     repo_name: @repository_data.name,
+  #     description: @repository_data.description,
+  #     default_branch: @repository_data.default_branch,
+  #     watchers_count: @repository_data.watchers_count,
+  #     language: @repository_data.language,
+  #     repo_created_at: @repository_data.created_at,
+  #     repo_updated_at: @repository_data.updated_at,
+  #   };
+
+  #   @new_repo = Repository.new(@new_repo_params)
+
+  #   if @new_repo.save
+  #     redirect_to repositories_path, notice: t('success')
+  #   else
+  #     flash[:notice] = t('fail')
+  #     render :new, status: :unprocessable_entity
+  #   end
+  #   # END
+  # end
+
+  # def edit
+  #   @repository = Repository.find params[:id]
+  # end
+
+  # def update
+  #   @repository = Repository.find params[:id]
+
+  #   if @repository.update(permitted_params)
+  #     redirect_to repositories_path, notice: t('success')
+  #   else
+  #     flash[:notice] = t('fail')
+  #     render :edit, status: :unprocessable_entity
+  #   end
+  # end
+
+  # teacher solution
   def create
-    # BEGIN
-    @link = permitted_params[:link]
-    @client = Octokit::Client.new
-    @octokit_repo = Octokit::Repository.from_url(@link)
-    @repository_data = @client.repository(@octokit_repo)
-    @new_repo_params = {
-      link: @link,
-      owner_name: @repository_data.owner.login,
-      repo_name: @repository_data.name,
-      description: @repository_data.description,
-      default_branch: @repository_data.default_branch,
-      watchers_count: @repository_data.watchers_count,
-      language: @repository_data.language,
-      repo_created_at: @repository_data.created_at,
-      repo_updated_at: @repository_data.updated_at,
-    };
+    @repository = Repository.new(permitted_params)
 
-    @new_repo = Repository.new(@new_repo_params)
+    if @repository.save
+      client = Octokit::Client.new
 
-    if @new_repo.save
-      redirect_to repositories_path, notice: t('success')
+      octokit_repo = Octokit::Repository.from_url(@repository.link)
+
+      github_data = client.repository(octokit_repo)
+
+      @repository.update!(
+        repo_name: github_data[:name],
+        owner_name: github_data[:owner][:login],
+        description: github_data[:description],
+        default_branch: github_data[:default_branch],
+        watchers_count: github_data[:watchers_count],
+        language: github_data[:language],
+        repo_created_at: github_data[:created_at],
+        repo_updated_at: github_data[:updated_at]
+      )
+      redirect_to @repository, notice: t('success')
     else
       flash[:notice] = t('fail')
       render :new, status: :unprocessable_entity
-    end
-    # END
-  end
-
-  def edit
-    @repository = Repository.find params[:id]
-  end
-
-  def update
-    @repository = Repository.find params[:id]
-
-    if @repository.update(permitted_params)
-      redirect_to repositories_path, notice: t('success')
-    else
-      flash[:notice] = t('fail')
-      render :edit, status: :unprocessable_entity
     end
   end
 
--- rails-full-ru/testing-2/test/controllers/web/repositories_controller_test.rb
+++ rails-full-ru/testing-2/test/controllers/web/repositories_controller_test.rb
@@ -4,16 +4,76 @@ require 'test_helper'
 
 class Web::RepositoriesControllerTest < ActionDispatch::IntegrationTest
   # BEGIN
-  test 'should_create' do
-    stub_response_data = {body: load_fixture('files/response.json')}
-    stub_request(:get, "http://[https//github.com%5D:80/alex-ismailov").
-      to_return_json(stub_response_data)
+  # test 'should_create' do
+  #   stub_response_data = {body: load_fixture('files/response.json')}
+  #   stub_request(:get, "http://[https//github.com%5D:80/alex-ismailov").
+  #     to_return_json(stub_response_data)
 
-    response = Net::HTTP.get('https://github.com', '/alex-ismailov')
+  #   response = Net::HTTP.get('https://github.com', '/alex-ismailov')
 
-    refute_nil(response)
-    assert_equal(stub_response_data[:body], response)
-    # byebug
-  end
+  #   refute_nil(response)
+  #   assert_equal(stub_response_data[:body], response)
+  #   # byebug
+  # end
   # END
+
+  # teacher solution
+  setup do
+    @repo = repositories :one
+
+    @attrs = {
+      link: 'https://github.com/railsware/js-routes'
+    }
+  end
+
+  test 'get index' do
+    get repositories_url
+    assert_response :success
+  end
+
+  test 'get new' do
+    get new_repository_url
+    assert_response :success
+  end
+
+  test 'should create' do
+    response = load_fixture('files/response.json')
+
+    stub_request(:get, 'https://api.github.com/repos/railsware/js-routes')
+      .to_return(
+        status: 200,
+        body: response,
+        headers: { 'Content-Type' => 'application/json' }
+      )
+
+    post repositories_url, params: { repository: @attrs }
+
+    repository = Repository.find_by @attrs
+
+    assert { repository }
+    assert { repository.description.present? }
+    assert_redirected_to repository_url(repository)
+  end
+
+  test 'get edit' do
+    get edit_repository_url(@repo)
+    assert_response :success
+  end
+
+  test 'should update' do
+    patch repository_url(@repo), params: { repository: @attrs }
+
+    @repo.reload
+
+    assert { @repo.link == @attrs[:link] }
+    assert_redirected_to repositories_url
+  end
+
+  test 'destroy' do
+    delete repository_url(@repo)
+
+    assert { !Repository.exists? @repo.id }
+
+    assert_redirected_to repositories_url
+  end
 end
