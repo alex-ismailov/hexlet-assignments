a28977a18d5a9e178c6e83c9bd599b09baaa3202
@hexlet/cli <support@hexlet.io>
2023-11-07 02:05:38 -0400

    submit rails-full-ru/caching

--- rails-full-ru/caching/app/models/article.rb
+++ rails-full-ru/caching/app/models/article.rb
@@ -2,20 +2,8 @@
 
 class Article < ApplicationRecord
   # BEGIN
-  # def competing_price
-  #   Rails.cache.fetch("#{cache_key_with_version}/competing_price", expires_in: 12.hours) do
-  #     Competitor::API.find_price(id)
-  #   end
-  # end
-
-  # def last_reading_date_DRAFT
-  #   Rails.cache.fetch("last_reading_date_#{id}", expires_in: 12.hours) do
-  #     Time.current
-  #   end
-  # end
-
   def last_reading_date
-    Rails.cache.fetch("#{cache_key_with_version}/artcle", expires_in: 12.hours) do
+    Rails.cache.fetch("#{cache_key_with_version}/last_reading_date", expires_in: 12.hours) do
       Time.current
     end
   end
